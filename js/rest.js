e8547215e4d136d8e40b753b9480388290cb7cc8
jasonr44 <jasonr44@sbcglobal.net>
2017-02-07 20:35:30 -0500

    Added some css and handlebars
    Added some css and handlebars

--- js/rest.js
+++ js/rest.js
@@ -1,5 +1,4 @@
 
-var commentsContainer = document.getElementById("comments");
 var btn = document.getElementById("btn");
 
 btn.addEventListener("click", function () {
@@ -7,10 +6,10 @@ btn.addEventListener("click", function () {
     ourRequest.open('GET', 'http://fe3.814.myftpupload.com/wp-json/wp/v2/comments');
     ourRequest.onload = function () {
         if (ourRequest.status >= 200 && ourRequest.status < 400) {
-         // console.log(ourRequest.responseText);
-        var ourData = JSON.parse(ourRequest.responseText);
-       // console.log(ourData[0]);
-       renderHTML(ourData);
+
+        var data = JSON.parse(ourRequest.responseText);
+     
+       createHTML(data);
         } else {
             console.log("we connnected but got an error");
         }
@@ -32,23 +31,12 @@ function renderHTML (data) {
     commentsContainer.insertAdjacentHTML('beforeend', htmlString);
 }
 
+function createHTML(data) {
+    var rawTemplate = document.getElementById("commentsTemplate").innerHTML;
+    var compiledTemplate = Handlebars.compile(rawTemplate);
+    var ourGeneratedHTML = compiledTemplate(data);
 
+    var commentsContainer = document.getElementById("commentsContainer");
+    commentsContainer.innerHTML = ourGeneratedHTML;
+}
 
-
-
-
-
-// var thePets = [
-//     {
-//     "name": "meowsalot",
-//     "species": "cat",
-//     "favFood": "tuna"
-//     },
-//     {
-//     "name": "Barky",
-//     "species": "dog",
-//     "favFood": "carrots"
-//     }
-// ]
-
-// 'thePets[1].favFood' would give you the value "carrots" 
